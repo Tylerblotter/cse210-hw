b62e532ff5befc46914596646b5249e70b7c8af8
Tyler Blotter <Tylerblotter906@gmail.com>
2023-01-11 09:34:10 -0700

    working on it

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"The sum is: {number} ");
+        
     }
 }
