cafa149c4a15f52a015966767b30ece292ec36f2
Tyler Blotter <Tylerblotter906@gmail.com>
2023-01-11 19:59:51 -0700

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,11 +7,41 @@ class Program
     {
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         List<int> numbers = new List<int>();
-        
-        Console.Write("Enter number: ");
-        int number = int.Parse(Console.ReadLine());
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+        Console.Write("Enter number (0 to end): ");
 
-        Console.WriteLine($"The sum is: {number} ");
-        
+        string userResponse = Console.ReadLine();
+        userNumber = int.Parse(userResponse);
+
+        if (userNumber != 0)
+        {
+            numbers.Add(userNumber);
+        }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"This sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
